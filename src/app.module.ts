813d8217990f55a41aa0289d868639cc088714c0
Bryla <csowscy@gmail.com>
2023-11-22 13:16:17 +0100

    Add test endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DatabaseModule } from "./database/database.module";
+import { TestModule } from "./test/test.module";
 
 @Module({
-	imports: [DatabaseModule],
+	imports: [DatabaseModule, TestModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
