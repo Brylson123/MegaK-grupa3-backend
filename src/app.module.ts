25ef8f5a80f671818baea47f73a44d9f2051f471
Bryla <csowscy@gmail.com>
2023-11-22 12:44:44 +0100

    add connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { DatabaseModule } from "./database/database.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [DatabaseModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
---
+++ src/database/database.provider.ts
@@ -0,0 +1,8 @@
+import { config } from "../config/config-database";
+
+export const databaseProviders = [
+	{
+		provide: "DATA_SOURCE",
+		useFactory: async () => config.initialize(),
+	},
+];
