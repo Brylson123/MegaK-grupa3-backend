606c676c592cf2e33a190c29f86a1d0b60e3dec5
Radek707 <rnowosielski77@gmail.com>
2023-11-23 03:32:25 +0100

    test endpoint

--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TestService } from "./test.service";
 import { TestController } from "./test.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Test } from "./test.entity";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
+	imports: [DatabaseModule],
 	controllers: [TestController],
 	providers: [TestService],
 })
