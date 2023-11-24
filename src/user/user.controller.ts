893525be73c1a422c8f89b13cb46560470709b63
Pr1mosz <olechprzemyslaw@gmail.com>
2023-11-24 11:01:34 +0100

    Add endpoint to get OneUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Param, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { UserEntity } from "./user.entity";
 
 @Controller("user")
 export class UserController {
 	constructor(@Inject(UserService) private userService: UserService) {}
+
+	@Get("/:id")
+	findOne(@Param("id") id: string): Promise<UserEntity> {
+		return this.userService.findOne(id);
+	}
 
 	@Post("/register")
 	addUser(@Body() newUser: CreateUserDto) {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,10 @@ import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UserService {
+	async findOne(id: string) {
+		return await UserEntity.findOne({ where: { id: id } });
+	}
+
 	async addUser(newUser: CreateUserDto): Promise<UserEntity> {
 		const user = new UserEntity();
 		user.email = newUser.email;
