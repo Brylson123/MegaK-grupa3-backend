569ecc900ec0fe0d8e0a21a7bc9bec1c680e459e
Pr1mosz <olechprzemyslaw@gmail.com>
2023-11-24 10:11:41 +0100

    Add register endpoint to add new User.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
 
-@Controller('user')
-export class UserController {}
+@Controller("user")
+export class UserController {
+	constructor(@Inject(UserService) private userService: UserService) {}
+
+	@Post("/register")
+	addUser(@Body() newUser: CreateUserDto) {
+		return this.userService.addUser(newUser);
+	}
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { UserEntity } from "./user.entity";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	async addUser(newUser: CreateUserDto): Promise<UserEntity> {
+		const user = new UserEntity();
+		user.email = newUser.email;
+		await user.save();
+		return user;
+	}
+}
