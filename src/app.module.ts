cf222ad309b9453f376b06e16b8d3d35725cf07c
Bryla <csowscy@gmail.com>
2023-11-27 01:03:55 +0100

    Add auth and reformat code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,11 @@ import { AppService } from "./app.service";
 import { DatabaseModule } from "./database/database.module";
 import { UserModule } from "./user/user.module";
 import { TestModule } from "./test/test.module";
-import { StudentModule } from './student/student.module';
-
+import { StudentModule } from "./student/student.module";
 
 @Module({
-	imports: [DatabaseModule, TestModule, StudentModule, UserModule], 
-  controllers: [AppController],
+	imports: [DatabaseModule, TestModule, StudentModule, UserModule],
+	controllers: [AppController],
 	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {cors: true});
-	app.useGlobalPipes(new ValidationPipe({
-		whitelist:true,
-		forbidNonWhitelisted:true,
-	  }));
+	const app = await NestFactory.create(AppModule, { cors: true });
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
 	await app.listen(3000);
 }
 bootstrap();
