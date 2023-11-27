0c969105d906959276611b8f58640d5ee8caed7d
Bryla <csowscy@gmail.com>
2023-11-27 01:25:17 +0100

    Add auth and decorators

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body, Res, Get, UseGuards } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthLoginDto } from "./dto/auth-login.dto";
+import { Response } from "express";
+import { AuthGuard } from "@nestjs/passport";
+import { UserEntity } from "../user/user.entity";
+import { UserObj } from "../decorators/user-obj.decorator";
+
+@Controller("auth")
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	@Post("/login")
+	async login(@Body() req: AuthLoginDto, @Res() res: Response): Promise<any> {
+		return this.authService.login(req, res);
+	}
+	@Get("logout")
+	@UseGuards(AuthGuard("jwt"))
+	async logout(@UserObj() user: UserEntity, @Res() res: Response) {
+		return this.authService.logout(user, res);
+	}
+}
---
+++ src/auth/dto/auth-login.dto.ts
@@ -0,0 +1,8 @@
+import { IsString } from "class-validator";
+
+export class AuthLoginDto {
+	@IsString()
+	email: string;
+	@IsString()
+	pwd: string;
+}
