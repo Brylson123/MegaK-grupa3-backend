a2c6bfd07afb60af91305ed58375921850359095
Bryła <csowscy@gmail.com>
2023-11-29 13:26:25 +0100

    Add hr endpoint to view, and filter students

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -4,19 +4,126 @@ import { StudentEntity } from "./entities/student.entity";
 import { BonusProjectUrl } from "./entities/bonusProjectUrls.entity";
 import { ProjectUrl } from "./entities/projectUrl.entity";
 import { PortfolioUrl } from "./entities/portfolioUrl.entity";
+import { ActiveStudentsDto } from "./dto/active-studnets.dto";
+import { config } from "../config/config-database";
+import {
+	StudentAvaibleViewInterface,
+	StudentInterface,
+	StudentStatus,
+	viewAllActiveStudentsResponse,
+} from "../types";
 
 @Injectable()
 export class StudentService {
-	async findAll() {
-		return await StudentEntity.find({
-			relations: {
-				projectUrls: true,
-				bonusProjectUrls: true,
-				portfolioUrls: true,
-				hrs: true,
-				user: true,
-			},
+	private filterAvaibleStudents = (student: StudentInterface[]): StudentAvaibleViewInterface[] => {
+		return student.map((student) => {
+			const {
+				id: studentId,
+				firstName,
+				lastName,
+				courseCompletion,
+				courseEngagement,
+				projectDegree,
+				teamProjectDegree,
+				expectedTypeWork,
+				targetWorkCity,
+				expectedContractType,
+				expectedSalary,
+				canTakeApprenticeship,
+				monthsOfCommercialExp,
+			} = student;
+			return {
+				studentId,
+				firstName,
+				lastName,
+				courseCompletion,
+				courseEngagement,
+				projectDegree,
+				teamProjectDegree,
+				expectedTypeWork,
+				targetWorkCity,
+				expectedContractType,
+				expectedSalary,
+				canTakeApprenticeship,
+				monthsOfCommercialExp,
+			};
 		});
+	};
+
+	async viewAllActiveStudents(req: ActiveStudentsDto): Promise<viewAllActiveStudentsResponse> {
+		try {
+			const {
+				pageSize,
+				currentPage,
+				courseCompletion,
+				courseEngagement,
+				projectDegree,
+				teamProjectDegree,
+				expectedTypeWork,
+				expectedContractType,
+				canTakeApprenticeship,
+				monthsOfCommercialExp,
+			} = req;
+
+			const searchTerm = req.search;
+			const expectedSalaryMin = req.expectedSalaryMin.length === 0 ? "0" : req.expectedSalaryMin;
+			const expectedSalaryMax = req.expectedSalaryMax.length === 0 ? "0" : req.expectedSalaryMin;
+
+			const [students, count] = await config
+				.getRepository(StudentEntity)
+				.createQueryBuilder()
+				.where(
+					'courseCompletion >= :courseCompletion AND courseEngagement >= :courseEngagement AND projectDegree >= :projectDegree AND teamProjectDegree >= :teamProjectDegree AND (canTakeApprenticeship = :canTakeApprenticeship OR canTakeApprenticeship = "Tak") AND monthsOfCommercialExp >= :monthsOfCommercialExp AND (expectedSalary BETWEEN :expectedSalaryMin AND :expectedSalaryMax OR expectedSalary IS null )',
+					{
+						courseCompletion,
+						courseEngagement,
+						projectDegree,
+						teamProjectDegree,
+						canTakeApprenticeship,
+						monthsOfCommercialExp,
+						expectedSalaryMin,
+						expectedSalaryMax,
+					},
+				)
+				.andWhere(
+					!expectedContractType[0]
+						? "status = :status"
+						: '(expectedContractType IN (:expectedContractType) OR expectedContractType = "Bez znaczenia")',
+					{
+						expectedContractType,
+					},
+				)
+				.andWhere(
+					!expectedTypeWork[0]
+						? "status = :status"
+						: '(expectedTypeWork IN (:expectedTypeWork) OR expectedTypeWork = "Bez znaczenia")',
+					{
+						status: StudentStatus.ACCESSIBLE,
+						expectedTypeWork,
+					},
+				)
+				.andWhere(
+					searchTerm.length === 0
+						? "status = :status"
+						: '(MATCH(targetWorkCity) AGAINST (":searchTerm*" IN BOOLEAN MODE) OR MATCH(expectedTypeWork) AGAINST (":searchTerm*" IN BOOLEAN MODE) OR MATCH(expectedContractType) AGAINST (":searchTerm*" IN BOOLEAN MODE))',
+					{
+						status: StudentStatus.ACCESSIBLE,
+						searchTerm,
+					},
+				)
+				.skip(pageSize * (currentPage - 1))
+				.take(pageSize)
+				.getManyAndCount();
+			const pageCount = Math.ceil(count / pageSize);
+			return {
+				isSuccess: true,
+				pageCount,
+				students: this.filterAvaibleStudents(students),
+				studentsCount: count,
+			};
+		} catch (e) {
+			throw new Error(e.message);
+		}
 	}
 
 	async findOne(id: string) {
@@ -39,7 +146,7 @@ export class StudentService {
 			student.bio = createStudentDto.bio;
 			student.canTakeApprenticeship = createStudentDto.canTakeApprenticeship;
 			student.courseCompletion = createStudentDto.courseCompletion;
-			student.courseEngagment = createStudentDto.courseEngagment;
+			student.courseEngagement = createStudentDto.courseEngagement;
 			student.courses = createStudentDto.courses;
 			student.education = createStudentDto.education;
 			student.expectedContractType = createStudentDto.expectedContractType;
@@ -81,7 +188,7 @@ export class StudentService {
 			student.bio = updateStudentDto.bio;
 			student.canTakeApprenticeship = updateStudentDto.canTakeApprenticeship;
 			student.courseCompletion = updateStudentDto.courseCompletion;
-			student.courseEngagment = updateStudentDto.courseEngagment;
+			student.courseEngagement = updateStudentDto.courseEngagement;
 			student.courses = updateStudentDto.courses;
 			student.education = updateStudentDto.education;
 			student.expectedContractType = updateStudentDto.expectedContractType;
