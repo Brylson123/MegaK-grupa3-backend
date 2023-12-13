f2b9cf8c40b8ba6f7ead1de1d03050fbf6a4d082
Pr1mosz <olechprzemyslaw@gmail.com>
2023-12-13 11:43:44 +0100

    Added unit test for user controller.

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,79 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
+import { UserEntity } from "./entity/user.entity";
 
 describe("UserController", () => {
 	let controller: UserController;
+	let userService: UserService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [UserController],
-			providers: [{ provide: UserService, useValue: {} }]
+			providers: [
+				{
+					provide: UserService,
+					useValue: {
+						findOne() {},
+						createUser() {},
+					},
+				},
+			],
 		}).compile();
 
 		controller = module.get<UserController>(UserController);
+		userService = module.get(UserService);
 	});
 
 	it("should be defined", () => {
 		expect(controller).toBeDefined();
+	});
+
+	it("should return user", async () => {
+		// Arrange
+		const user = new UserEntity();
+		Object.assign(user, { id: "1234", email: "test@example.com" });
+		jest.spyOn(userService, "findOne").mockImplementation(async () => user);
+
+		// Act
+		const result = await controller.findOne("1234");
+
+		// Assert
+		expect(result).toEqual({
+			id: "1234",
+			email: "test@example.com",
+		});
+		expect(userService.findOne).toHaveBeenCalledWith("1234");
+	});
+
+	it("should create user", async () => {
+		const user = new UserEntity();
+		Object.assign(user, {
+			id: "1234",
+			email: "test@example.com",
+			pwdHash: "fsefsve",
+			salt: "sfefscvs",
+			currentTokenId: null,
+			activeToken: "token1234",
+			active: false,
+			student: null,
+			hr: null,
+		});
+		jest.spyOn(userService, "createUser").mockImplementation(async () => user);
+
+		const result = await controller.createUser(user);
+
+		expect(result).toEqual({
+			id: "1234",
+			email: "test@example.com",
+			pwdHash: "fsefsve",
+			salt: "sfefscvs",
+			currentTokenId: null,
+			activeToken: "token1234",
+			active: false,
+			student: null,
+			hr: null,
+		});
+		expect(userService.createUser).toHaveBeenCalledWith(user);
 	});
 });
