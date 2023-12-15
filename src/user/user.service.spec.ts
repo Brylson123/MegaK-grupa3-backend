9d5ca037c274137c30c442aada5b0b2d01067da2
Pr1mosz <olechprzemyslaw@gmail.com>
2023-12-15 15:56:09 +0100

    Add UserService findOne test.

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,16 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserService } from "./user.service";
 import { MailService } from "../mail/mail.service";
+import { UserEntity } from "./entity/user.entity";
 
 describe("UserService", () => {
 	let service: UserService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [
-				UserService,
-				{provide: MailService, useValue: {}}
-			],
+			providers: [UserService, { provide: MailService, useValue: {} }],
 		}).compile();
 
 		service = module.get<UserService>(UserService);
@@ -18,5 +16,20 @@ describe("UserService", () => {
 
 	it("should be defined", () => {
 		expect(service).toBeDefined();
+	});
+
+	describe("findOne", () => {
+		it("should return user", async () => {
+			const user = new UserEntity();
+			Object.assign(user, { id: "1234" });
+
+			jest.spyOn(UserEntity, "findOne").mockResolvedValueOnce(user);
+
+			const result = await service.findOne("1234");
+
+			expect(result).toEqual({
+				id: "1234",
+			});
+		});
 	});
 });
